0d277eef76da5b1b5cd25e397640b29caaedb8eb
joonasstengard <joonas.stengard@gmail.com>
2026-08-14 21:58:20 +0300

    Add new items

--- lib/items/itemIds.ts
+++ lib/items/itemIds.ts
@@ -3,6 +3,17 @@ export const ITEM_ID = {
   bloodseal: 2,
   moonshardRing: 3,
   emberdelveSignet: 4,
+  thalenEmeraldRing: 5,
+  amberSignet: 6, 
+  nightglassRing: 7,
+  steelBand: 8,
+  threeMarks: 9,
+  ritualistsBracelet: 10,
+  seresTear: 11,
+  graveCharm: 12,
+  woundBead: 13,
+  palesilverBelt: 14,
+  leatherBelt: 15,
 } as const;
 
 export type ItemId = (typeof ITEM_ID)[keyof typeof ITEM_ID];
